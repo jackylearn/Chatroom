0142b3863ce063fb1321a5401d0583a99f10ae91
Jacky <jacky3627@gmail.com>
2021-08-16 08:55:01 +0800

    update field name for username
    from "name" to "username"

--- auth.js
+++ auth.js
@@ -42,7 +42,7 @@ module.exports = (app, myDataBase) => {
         {
           $setOnInsert: {
             id: profile.id,
-            name: profile.displayName || 'John Doe',
+            username: profile.displayName || 'John Doe',
             photo: profile.photos[0].value || '',
             email: Array.isArray(profile.emails) ? profile.emails[0].value : 'No public email',
             created_on: new Date(),
